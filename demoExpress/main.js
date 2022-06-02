4494a199bff3a41f87ae8d0706e04d6e80ea01ad
DoQuocBinh <binhdq@fpt.edu.vn>
2022-06-02 12:05:52 +0700

    demo 2 complete

--- demoExpress/main.js
+++ demoExpress/main.js
@@ -4,11 +4,20 @@ var app = express()
 
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
+2
+const foods = ['my tom','pho','xoi ga']
 
 app.post('/registerLunch',function(req,res){
     //1.lay gia tri nguoi dung nhap o Form
     let name = req.body.txtName
     let food = req.body.food
+    //1.1 kiem tra ten co ngan qua khong
+    if(name.length <3){
+        //Bao loi
+        let errorMsg = "Ten qua ngan"
+        res.render('student',{'errorMsg':errorMsg,'foods':foods})
+        return
+    }
     //2. Save vao file: name va food
     let data = name + ';' + food + '\n'
     fs.appendFile('data.txt',data,function(){})
@@ -28,8 +37,8 @@ app.get('/',function (req,res){
     res.render('home',{'now':now,'name':name})
 })
 
-app.get('/student',function(req,res){
-    let foods = ['my tom','pho','xoi ga']
+
+app.get('/student',function(req,res){    
     res.render('student',{'foods': foods})
 })
 
