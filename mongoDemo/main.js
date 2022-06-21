680af7ceeaa32502acd69ec7c6428ca88b6e97a4
DoQuocBinh <binhdq@fpt.edu.vn>
2022-06-21 11:36:57 +0700

    ok

--- mongoDemo/main.js
+++ mongoDemo/main.js
@@ -9,9 +9,21 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 //duong dan den database
-var url = 'mongodb://127.0.0.1:27017';
+var url = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin'
 //import thu vien MongoDB
 var MongoClient = require('mongodb').MongoClient;
+
+app.post('/search', async (req,res)=>{
+    let nameSearch = req.body.txtName
+    //1.ket noi den database server voi dia chi la url
+    let client= await MongoClient.connect(url);
+    //2.truy cap database ATNToys
+    let dbo = client.db("ATNToys");
+    //tra ve toan bo bang product
+    let products = await dbo.collection("product").find({'name': new RegExp(nameSearch,'i')}).toArray()
+    //hien thi trang viewProduct voi Product trong Database tra ve
+    res.render('allProduct',{'products':products})
+})
 
 app.get('/viewAll',async (req,res)=>{
     //1.ket noi den database server voi dia chi la url
@@ -28,6 +40,10 @@ app.post('/createProduct',async (req,res)=>{
     let name = req.body.txtName
     let price =req.body.txtPrice
     let picURL = req.body.txtPicture
+    if(name.length <= 6){
+        res.render('createProduct',{'nameError':'Ten khong nho hon 5 ky tu!'})
+        return
+    }
     let product = {
         'name':name,
         'price': price,
