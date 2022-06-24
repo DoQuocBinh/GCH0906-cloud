1d84366a56baa6ee14d58fcf98996110006c2dc5
DoQuocBinh <binhdq@fpt.edu.vn>
2022-06-24 08:47:45 +0700

    update row

--- mongoDemo/main.js
+++ mongoDemo/main.js
@@ -3,10 +3,10 @@ const async = require('hbs/lib/async')
 const { ObjectId } = require('mongodb')
 var app = express()
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
 //duong dan den database
@@ -14,45 +14,75 @@ var url = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27
 //import thu vien MongoDB
 var MongoClient = require('mongodb').MongoClient;
 
-app.post('/search', async (req,res)=>{
+app.post('/editProduct', async (req, res) => {
+    let id = req.body._id
+    let name = req.body.txtName
+    let price = req.body.txtPrice
+    let picURL = req.body.txtPicture
+    //1.ket noi den database server voi dia chi la url
+    let client = await MongoClient.connect(url);
+    //2.truy cap database ATNToys
+    let dbo = client.db("ATNToys")
+    await dbo.collection("product").updateOne({ _id:ObjectId(id) }, {
+        $set:
+            { 'name': name, 'price': price, 'picURL': picURL }
+    })
+    res.redirect('/')
+
+})
+
+app.get('/edit', async (req, res) => {
+    let id = req.query.id
+    //1.ket noi den database server voi dia chi la url
+    let client = await MongoClient.connect(url);
+    //2.truy cap database ATNToys
+    let dbo = client.db("ATNToys");
+    //tra ve product co id nguoi dung can Edit
+    let product = await dbo.collection("product").findOne({ _id: ObjectId(id) })
+    console.log(product)
+    res.render('edit', { 'product': product })
+})
+
+
+app.post('/search', async (req, res) => {
     let nameSearch = req.body.txtName
     //1.ket noi den database server voi dia chi la url
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     //2.truy cap database ATNToys
     let dbo = client.db("ATNToys");
     //tra ve toan bo bang product
-    let products = await dbo.collection("product").find({$or:[{'name': new RegExp(nameSearch,'i')},{_id: nameSearch}]}).toArray()
+    let products = await dbo.collection("product").find({ $or: [{ 'name': new RegExp(nameSearch, 'i') }, { _id: nameSearch }] }).toArray()
     //hien thi trang viewProduct voi Product trong Database tra ve
-    res.render('allProduct',{'products':products})
+    res.render('allProduct', { 'products': products })
 })
 
-app.get('/viewAll',async (req,res)=>{
+app.get('/viewAll', async (req, res) => {
     //1.ket noi den database server voi dia chi la url
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     //2.truy cap database ATNToys
     let dbo = client.db("ATNToys");
     //tra ve toan bo bang product
     let products = await dbo.collection("product").find().toArray()
     //hien thi trang viewProduct voi Product trong Database tra ve
-    res.render('allProduct',{'products':products})
+    res.render('allProduct', { 'products': products })
 })
 
-app.post('/createProduct',async (req,res)=>{
+app.post('/createProduct', async (req, res) => {
     let name = req.body.txtName
-    let price =req.body.txtPrice
+    let price = req.body.txtPrice
     let picURL = req.body.txtPicture
-    if(name.length <= 6){
-        res.render('createProduct',{'nameError':'Ten khong nho hon 5 ky tu!'})
+    if (name.length <= 6) {
+        res.render('createProduct', { 'nameError': 'Ten khong nho hon 5 ky tu!' })
         return
     }
     let product = {
-        'name':name,
+        'name': name,
         'price': price,
-        'picURL':picURL
+        'picURL': picURL
     }
     //insert product vao database
     //1.ket noi den database server voi dia chi la url
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     //2.truy cap database ATNToys
     let dbo = client.db("ATNToys");
     //3.insert product vao database ATNToys, trong table product
@@ -61,7 +91,7 @@ app.post('/createProduct',async (req,res)=>{
     res.redirect('/')
 })
 
-app.get('/create',(req,res)=>{
+app.get('/create', (req, res) => {
     res.render('createProduct')
 })
 
--- sessionDemo/main.js
+++ sessionDemo/main.js
@@ -1,25 +1,52 @@
 var express = require('express')
 var app = express()
 var session = require('express-session')
+var mongoClient = require('mongodb').MongoClient
 
-app.set('view engine','hbs')
+var url = 'mongodb://127.0.0.1/27017'
+
+app.set('view engine', 'hbs')
 app.use(session({
-    secret : '##%@#$^^&**(###GHH##@D',
+    secret: '##%@#$^^&**(###GHH##@D',
     resave: false
 }))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/register',(req,res)=>{
+app.post('/register', async (req, res) => {
     let name = req.body.txtName
-    req.session.userName = name
+    //req.session.userName = name
+    let server = await mongoClient.connect(url)
+    let dbo = server.db("ATNToys")
+    let user = await dbo.collection("users").find({ 'name': name }).toArray()
+    if (user.length > 0)
+        req.session.userName = user[0].name
     res.redirect('/')
 })
 
-app.get('/',(req,res)=>{
+function isAuthenticated(req, res, next) {
+    if (!req.session.userName)
+        res.redirect('/')
+    else
+        next()
+}
+app.get('/profile',isAuthenticated, (req, res) => {
+    res.render('profile', { name: req.session.userName })
+})
+
+app.get('/logout',isAuthenticated,(req,res)=>{
+    req.session.userName = null
+    req.session.save((err)=>{
+        req.session.regenerate((err2)=>{
+            res.redirect('/')
+        })
+    })
+})
+
+app.get('/', (req, res) => {
     var count = req.session.count || 0
     count++
     req.session.count = count
-    res.render('home',{'count':count,'name': req.session.userName})
+    res.render('home', { 'count': count, 'name': req.session.userName })
 })
 
 const PORT = process.env.PORT || 5000
